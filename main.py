22ec5ea0be8ba90b75c77882f9647c9a5bce6f06
AwsManas <manassharma6301@gmail.com>
2019-10-24 08:07:48 +0530

    Connected

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-from flask import Flask , request , render_template
-
+from flask import Flask , request , render_template, redirect
+from flask_mysqldb import MySQL
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("signup.html")
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'M@nas4life'
+app.config['MYSQL_DB'] = 'nie'
 
+mysql = MySQL(app)
+
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+        inputvalues = request.form
+        name = inputvalues['name']
+        usn = inputvalues['USN']
+        email = inputvalues['email']
+        phone = int(inputvalues['phone_number'])
+        sem = int(inputvalues['sem'])
+        passw = inputvalues['pass']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO signup VALUES (%s,%s,%s,%s,%s,%s)", (name,usn,email,phone,sem,passw))
+        mysql.connection.commit()
+        cur.close()
+    return render_template("signup.html")
 if __name__ == "__main__":
     app.run(debug=True)
 
