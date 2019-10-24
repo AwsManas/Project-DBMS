05631e575b8004fbf5a57854059199b579781a14
AwsManas <manassharma6301@gmail.com>
2019-10-24 09:09:04 +0530

    login.html

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ def index():
         mysql.connection.commit()
         cur.close()
     return render_template("signup.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True)
 
